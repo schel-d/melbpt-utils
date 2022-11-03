46127f6ee5d4ae203e2cb713d8e9fd9b10ef3510
schel-d <schel.d.dev@gmail.com>
2022-11-03 15:43:42 +1100

    generalise section index counting procedure

--- ts/ttbl/ttbl-convert.ts
+++ ts/ttbl/ttbl-convert.ts
@@ -1,7 +1,8 @@
 import { Timetable } from "../timetable/timetable";
 import { TimetableEntryStop, TimetableEntryWithinSection }
   from "../timetable/timetable-entry";
-import { isTimetableEntryIndex } from "../timetable/timetable-entry-index";
+import { isTimetableEntryIndex, TimetableEntryIndex }
+  from "../timetable/timetable-entry-index";
 import { TimetableError } from "../timetable/timetable-error";
 import { TimetableSection } from "../timetable/timetable-section";
 import { LocalTime } from "../utils/local-time";
@@ -19,18 +20,14 @@ import { kebabify } from "./utils";
  * @param ttbl The parsed .ttbl file.
  */
 export function timetableFromTtbl(ttbl: TtblFile): Timetable {
-  // Each grid section becomes a timetable section.
-  const sections = [];
-  let currIndex = 0;
-
-  for (const grid of ttbl.grids) {
-    // Map each service into a entry for the timetable section.
+  const sections = toSections(ttbl.grids, (grid, firstIndex) => {
     const entries = [];
+
+    // For every column in the grid...
     for (let x = 0; x < grid.width; x++) {
+      // Convert the rows into timetable entry stops (removing empty rows).
       const times = grid.rows
-        .map(r => {
-          return { stop: r.stop, time: r.times[x] };
-        })
+        .map(r => { return { stop: r.stop, time: r.times[x] }; })
         .filter(t => t.time != null)
         .map(x => new TimetableEntryStop(x.stop, x.time as LocalTime));
 
@@ -38,24 +35,47 @@ export function timetableFromTtbl(ttbl: TtblFile): Timetable {
       entries.push(entry);
     }
 
+    // Create the section using the provided first index.
+    return new TimetableSection(
+      grid.direction, grid.wdr, firstIndex, entries
+    );
+  });
+
+  return new Timetable(
+    ttbl.id, ttbl.line, ttbl.created, ttbl.type, ttbl.begins, ttbl.ends,
+    sections
+  );
+}
+
+type Converter<T> =
+  (obj: T, firstIndex: TimetableEntryIndex) => TimetableSection;
+
+/**
+ * A generalised version of the logic that creates {@link TimetableSection}s and
+ * counts the number of entries as it goes to provide the starting index for the
+ * next section.
+ * @param objs The objects to convert into timetable sections.
+ * @param func The function which takes an object and first index, and creates a
+ * {@link TimetableSection}.
+ */
+export function toSections<T>(objs: T[], func: Converter<T>): TimetableSection[] {
+  const sections = [];
+  let currIndex = 0;
+
+  for (const obj of objs) {
     // Individual sections make sure they're not overflowing, but if the last
     // section fits perfectly within the bounds, we'd now be one over, so check.
     if (!isTimetableEntryIndex(currIndex)) {
       throw TimetableError.tooManyServices();
     }
 
-    const section = new TimetableSection(
-      grid.direction, grid.wdr, currIndex, entries
-    );
+    const section = func(obj, currIndex);
     sections.push(section);
 
     (currIndex as number) += section.entriesCount;
   }
 
-  return new Timetable(
-    ttbl.id, ttbl.line, ttbl.created, ttbl.type, ttbl.begins, ttbl.ends,
-    sections
-  );
+  return sections;
 }
 
 /**
